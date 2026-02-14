43197d5b0d6a33fcf3811d9c35b6b09d526d50f0
glitch1g <zaki910777@gmail.com>
2026-02-14 09:06:41 +0800

    Delete js directory

--- js/utils.js
+++
@@ -1,8 +0,0 @@
-/* ============================================
-   UTILS
-   ============================================ */
-
-
-    // ============ CONSTANTS ============
-    const PASSWORD = 'loveu'; // Ganti dengan password yang kamu mau
-    const RELATIONSHIP_START_DATE = new Date('2026-01-09'); // Tanggal mulai: 9 Januari 2026
